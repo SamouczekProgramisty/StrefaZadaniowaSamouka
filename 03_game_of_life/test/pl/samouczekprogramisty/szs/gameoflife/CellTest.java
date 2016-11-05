0af2a0f238c94c18c43c3a168857eb019a1499d5
Marcin Pietraszek <m.pietraszek@gmail.com>
2016-11-05 16:00:06 +0100

    Cell should be able to return its neighbours - failing test case

--- 03_game_of_life/test/pl/samouczekprogramisty/szs/gameoflife/CellTest.java
+++ 03_game_of_life/test/pl/samouczekprogramisty/szs/gameoflife/CellTest.java
@@ -2,6 +2,9 @@ package pl.samouczekprogramisty.szs.gameoflife;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class CellTest {
@@ -16,5 +19,25 @@ public class CellTest {
     public void shouldBeImmutable() {
         Cell cell = Cell.live(0, 0);
         assertNotSame(cell, cell.flip());
+    }
+
+    @Test
+    public void shouldBeAbleToReturnNeighbours() {
+        Cell cell00 = Cell.dead(0, 0);
+        Cell cell01 = Cell.dead(0, 1);
+        Cell cell02 = Cell.dead(0, 2);
+        Cell cell10 = Cell.dead(1, 0);
+        Cell cell11 = Cell.dead(1, 1);
+        Cell cell12 = Cell.dead(1, 2);
+        Cell cell20 = Cell.dead(2, 0);
+        Cell cell21 = Cell.dead(2, 1);
+        Cell cell22 = Cell.dead(2, 2);
+
+        Board board = new Board(3, cell00, cell01, cell02, cell10, cell11, cell12, cell20, cell21, cell22);
+
+        List<Cell> neighbours = cell11.getNeighbours(board);
+        List<Cell> expected = Arrays.asList(cell00, cell01, cell02, cell10, cell12, cell20, cell21, cell22);
+
+        assertEquals(expected, neighbours);
     }
 }
