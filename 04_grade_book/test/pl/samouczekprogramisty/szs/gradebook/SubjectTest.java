5496b52c985f5f836c85ce07920007946e9352b5
Marcin Pietraszek <m.pietraszek@gmail.com>
2016-11-20 20:24:18 +0100

    #6 - RED Computing average note for a subject

--- 04_grade_book/test/pl/samouczekprogramisty/szs/gradebook/SubjectTest.java
+++ 04_grade_book/test/pl/samouczekprogramisty/szs/gradebook/SubjectTest.java
@@ -1,7 +1,9 @@
 package pl.samouczekprogramisty.szs.gradebook;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class SubjectTest {
@@ -9,5 +11,13 @@ public class SubjectTest {
     public void shoudBeAbleToCreateSubject() {
         Subject subject = new Subject("matematyka");
         assertNotNull(subject);
+    }
+
+    @Test
+    public void shouldBeAbleToComputeAverageNote() {
+        Subject subject = new Subject("matematyka");
+        subject.addNote(4);
+        subject.addNote(5);
+        assertEquals(4.5, subject.getAverage(), 0.001);
     }
 }
