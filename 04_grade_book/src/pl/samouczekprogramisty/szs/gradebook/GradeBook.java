c86f07d5fa57dfb2cfb376622884f634b6318ecb
Marcin Pietraszek <m.pietraszek@gmail.com>
2016-11-20 19:50:16 +0100

    #3 - GREEN Adding subject to GradeBook

--- 04_grade_book/src/pl/samouczekprogramisty/szs/gradebook/GradeBook.java
+++ 04_grade_book/src/pl/samouczekprogramisty/szs/gradebook/GradeBook.java
@@ -1,5 +1,12 @@
 package pl.samouczekprogramisty.szs.gradebook;
 
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class GradeBook {
+    private List<Subject> subjects = new LinkedList<>();
+    public void addSubject(Subject subject) {
+        subjects.add(subject);
+    }
 }
