cb3ebd00a359ea76e8aa09a26ac2876f605f253f
Marcin Pietraszek <m.pietraszek@gmail.com>
2016-11-20 20:35:45 +0100

    #7 - REFACTOR Computing global average note

--- 04_grade_book/test/pl/samouczekprogramisty/szs/gradebook/GradeBookTest.java
+++ 04_grade_book/test/pl/samouczekprogramisty/szs/gradebook/GradeBookTest.java
@@ -9,6 +9,8 @@ import static org.junit.Assert.assertNotNull;
 public class GradeBookTest {
 
     private static final String SUBJECT = "matematyka";
+    private static final String OTHER_SUBJECT = "fizyka";
+
     private GradeBook gradeBookWithSubject;
 
     @Before
@@ -53,11 +55,11 @@ public class GradeBookTest {
 
     @Test
     public void shouldComputeGlobalAverageNote()  {
-        gradeBookWithSubject.addSubject("fizyka");
+        gradeBookWithSubject.addSubject(OTHER_SUBJECT);
         gradeBookWithSubject.addNote(SUBJECT, 3);
         gradeBookWithSubject.addNote(SUBJECT, 5);
-        gradeBookWithSubject.addNote("fizyka", 2);
-        gradeBookWithSubject.addNote("fizyka", 4);
+        gradeBookWithSubject.addNote(OTHER_SUBJECT, 2);
+        gradeBookWithSubject.addNote(OTHER_SUBJECT, 4);
 
         assertEquals(3.5, gradeBookWithSubject.getAverageNote(), 0.001);
     }
