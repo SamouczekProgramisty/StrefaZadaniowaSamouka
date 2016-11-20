2607a9ef1152214561888931f7de9f107f215137
Marcin Pietraszek <m.pietraszek@gmail.com>
2016-11-20 19:55:33 +0100

    #3.1 - GREEN Adding subject to GradeBook

--- 04_grade_book/src/pl/samouczekprogramisty/szs/gradebook/GradeBook.java
+++ 04_grade_book/src/pl/samouczekprogramisty/szs/gradebook/GradeBook.java
@@ -8,6 +8,9 @@ public class GradeBook {
     private Map<String, Subject> subjects = new HashMap<>();
 
     public void addSubject(String subject) {
+        if (subjects.containsKey(subject)) {
+            throw new IllegalStateException();
+        }
         subjects.put(subject, new Subject(subject));
     }
 }
