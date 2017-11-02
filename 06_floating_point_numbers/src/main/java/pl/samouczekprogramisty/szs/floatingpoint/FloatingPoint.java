5f84a786235c03c79f63c67ab0144d3439adc3db
Marcin Pietraszek <m.pietraszek@gmail.com>
2017-11-02 23:06:01 +0100

    Class showing difference between IEEE756 implementation and actual number value.

---
+++ 06_floating_point_numbers/src/main/java/pl/samouczekprogramisty/szs/floatingpoint/FloatingPoint.java
@@ -0,0 +1,107 @@
+package pl.samouczekprogramisty.szs.floatingpoint;
+
+import java.math.BigDecimal;
+
+public class FloatingPoint {
+
+    public enum Format {
+        SINGLE_PRECISION,
+        DOUBLE_PRECISION
+    }
+
+    public enum Sign {
+        POSITIVE,
+        NEGATIVE
+    }
+
+    private final Format format;
+
+    private final Sign sign;
+    private final String exponent;
+    private final String fraction;
+
+    private final BigDecimal realValue;
+    private final BigDecimal ieee754Value;
+
+    public static FloatingPoint instantiate(Format format, String decimalRepresentation) {
+        switch (format) {
+            case DOUBLE_PRECISION:
+                return new FloatingPoint(
+                    new BigDecimal(decimalRepresentation),
+                    Double.parseDouble(decimalRepresentation)
+                );
+            default:
+                return new FloatingPoint(
+                    new BigDecimal(decimalRepresentation),
+                    Float.parseFloat(decimalRepresentation)
+                );
+        }
+    }
+
+    private FloatingPoint(Format format, BigDecimal realValue, BigDecimal ieee754Value, String binaryRepresentation) {
+        sign = Sign.values()[Integer.parseInt(binaryRepresentation.substring(0, 1))];
+        this.format = format;
+        this.realValue = realValue;
+        this.ieee754Value = ieee754Value;
+
+        switch (format) {
+            case SINGLE_PRECISION:
+                exponent = binaryRepresentation.substring(1, 1 + 8);
+                fraction = binaryRepresentation.substring(1 + 8);
+                break;
+            default:
+                exponent = binaryRepresentation.substring(1, 1 + 11);
+                fraction = binaryRepresentation.substring(1 + 11);
+                break;
+        }
+    }
+
+    private FloatingPoint(BigDecimal realValue, float ieee754Value) {
+        this(
+                Format.SINGLE_PRECISION,
+                realValue,
+                new BigDecimal(ieee754Value),
+                String.format(
+                        "%32s",
+                        Integer.toBinaryString(Float.floatToRawIntBits(ieee754Value))
+                ).replace(' ', '0')
+        );
+    }
+
+    private FloatingPoint(BigDecimal realValue, double ieee754Value) {
+        this(
+                Format.SINGLE_PRECISION,
+                realValue,
+                new BigDecimal(ieee754Value),
+                String.format(
+                        "%64s",
+                        Long.toBinaryString(Double.doubleToRawLongBits(ieee754Value))
+                ).replace(' ', '0')
+        );
+    }
+
+    public Format getFormat() {
+        return format;
+    }
+
+    public Sign getSign() {
+        return sign;
+    }
+
+    public String getExponent() {
+        return exponent;
+    }
+
+    public String getFraction() {
+        return fraction;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s %s", sign.ordinal(), exponent, fraction);
+    }
+
+    public BigDecimal getError() {
+        return realValue.subtract(ieee754Value).abs();
+    }
+}
---
+++ 06_floating_point_numbers/src/test/java/pl/samouczekprogramisty/szs/floatingpoint/TestFloatingPoint.java
@@ -0,0 +1,30 @@
+package pl.samouczekprogramisty.szs.floatingpoint;
+
+import org.junit.Test;
+import pl.samouczekprogramisty.szs.floatingpoint.FloatingPoint.Format;
+
+import java.math.BigDecimal;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class TestFloatingPoint {
+
+    @Test
+    public void shouldBeAbleToParseSinglePrecisionFloatingPointNumber() {
+        assertThat(FloatingPoint.instantiate(Format.SINGLE_PRECISION, "0.1").toString(), is(equalTo("0 01111011 10011001100110011001101")));
+    }
+
+    @Test
+    public void shouldBeAbleToParseDoublePrecisionFloatingPointNumber() {
+        assertThat(FloatingPoint.instantiate(Format.DOUBLE_PRECISION, "0.1").toString(), is(equalTo("0 01111111011 1001100110011001100110011001100110011001100110011010")));
+    }
+
+    @Test
+    public void shouldBeAbleToShowErrorDuringConversion() {
+        assertThat(FloatingPoint.instantiate(Format.SINGLE_PRECISION, "0.1").getError(), is(greaterThan(BigDecimal.ZERO)));
+    }
+
+}
