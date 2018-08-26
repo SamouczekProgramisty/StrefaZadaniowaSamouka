2690462d2d809edb4c873f0a3cb2d823299bb637
Marcin Pietraszek <m.pietraszek@gmail.com>
2018-08-26 19:58:10 +0200

    Small refactoring with another set of basic tests

--- 09_structure_filtering/src/test/java/pl/samouczekprogramisty/szs/filtering/MyStructureTest.java
+++ 09_structure_filtering/src/test/java/pl/samouczekprogramisty/szs/filtering/MyStructureTest.java
@@ -1,15 +1,38 @@
 package pl.samouczekprogramisty.szs.filtering;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 class MyStructureTest {
 
+    private MyStructure structure;
+
+    @BeforeEach
+    void setUp() {
+        structure = new MyStructure();
+    }
+
     @Test
     void shouldBeAbleToInstantiateClass() {
-        IMyStructure structure = new MyStructure();
-        assertNotNull(structure);
+        assertThat(structure, notNullValue());
+    }
+
+    @Test
+    void shouldReturnNullWhenNotFoundByCode() {
+        assertThat(structure.findByCode("xxx"), is(nullValue()));
+    }
+
+    @Test
+    void shouldReturnNullWhenNotFoundByRenderer() {
+        assertThat(structure.findByRenderer("xxx"), is(nullValue()));
+    }
+
+    @Test
+    void shouldReturn0WhenStructureIsEmpty() {
+        assertThat(structure.count(), is(equalTo(0)));
     }
 
 }
